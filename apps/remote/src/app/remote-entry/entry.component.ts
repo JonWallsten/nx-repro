77ff0d2c2247cb7aaee4cb90d8f63b8791fdd2f3
Jon Wallsten <jon.wallsten@gmail.com>
2023-09-12 16:06:48 +0200

    Repro Nx Bug

---
+++ apps/remote/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'debug-remote-entry',
+  template: `<debug-nx-welcome></debug-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
